82bc25efc676d2b80c8b242c3cbbb8f4c56a6f77
Thomas Hinterecker# <t.hinterecker@gmail.com>
2018-01-07 15:06:47 +0100

    L2 fix

--- mnist_example.py
+++ mnist_example.py
@@ -29,7 +29,7 @@ from ArtNet.models import Sequential
 from ArtNet.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Activation, Dropout, BatchNormalization
 from ArtNet.activations import ReLU, Sigmoid, Softmax, Tanh, Leaky_ReLU, Linear
 from ArtNet.regularizers import L2
-from ArtNet.losses import MeanSquaredError, BinaryCrossEntropy, CategoricalCrossEntropy
+from ArtNet.losses import Loss, MeanSquaredError, BinaryCrossEntropy, CategoricalCrossEntropy
 from ArtNet.optimizers import GradientDescent, RMSprop, Adam
 from ArtNet.lib import r_squared
 
@@ -40,11 +40,11 @@ model.add(Dense(nodes=128, activation=ReLU(), weight_regularizer=L2()))
 #model.add(Conv2D(filters=1, kernel_size=(3, 3), padding="same", activation="ReLU", weight_initializer='GlorotUniform')) #
 #model.add(Activation(ReLU()))
 #model.add(MaxPooling2D(strides=(2, 2)))
-#model.add(Flatten())
-#model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dense(nodes=128, activation=ReLU()))
 model.add(Dense(nodes=10, activation=Softmax()))
 model.compile(loss=CategoricalCrossEntropy(), optimizer=Adam(), metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=2, batch_size=500, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=2, batch_size=200, validation_data=(x_test, y_test))
 
 #y_pred = model.predict(x_test, batch_size=500, verbose=0)
 #print("Accuracy:", (1 - np.sum(np.abs(y_test - np.round(np.squeeze(y_pred)))) / y_test.shape[0]) * 100, '%')
@@ -64,8 +64,8 @@ model.fit(x_train, y_train, epochs=2, batch_size=500, validation_data=(x_test, y
 # model.add(Flatten())
 # model.add(Dense(128, activation="relu"))
 # model.add(Dense(10, activation='softmax'))
-# model.compile(loss='categorical_crossentropy', optimizer="sgd", metrics=["accuracy"])
-# model.fit(x_train, y_train, epochs=2, batch_size=500, validation_data=(x_test, y_test))
+# model.compile(loss='categorical_crossentropy', optimizer="Adam", metrics=["accuracy"])
+# model.fit(x_train, y_train, epochs=2, batch_size=200, validation_data=(x_test, y_test))
 
 # y_pred = model.predict(x_test, batch_size=500, verbose=0)
 # print("Accuracy:", np.round((1 - np.sum(np.argmax(y_test, axis=1) != np.argmax(y_pred, axis=1)) / x_test.shape[0]) * 100, 2), "%")
